5c055221a755fd64706720476a78b2eb0d0b7195
ghgltggamer <109077342+12356544567875@users.noreply.github.com>
2023-05-20 19:40:39 +0530

    Add files via upload

---
+++ connection.js
@@ -0,0 +1,57 @@
+function home(){
+  window.open("index.html");
+}
+
+function downloads(){
+  window.open("Download.html");
+}
+
+function community(){
+  window.open("Community.html");
+}
+
+function license(){
+  window.open("License.html");
+}
+
+function news(){
+  window.open("news.html")
+}
+
+function fozdtx(){
+  window.open("https://fozdtxonline.wordpress.com");
+}
+
+function gws(){
+  window.open("https://gws.code.blog");
+}
+
+function buildcoderinside(){
+  window.open("https://www.buildcoderinsid.repl.co");
+}
+
+function ourplat(){
+  window.open("Our Platforms.html");
+}
+function developermode(){
+  // alert("Developer mode is in testing");
+  window.open("https://developermode.ghgltggamer.repl.co");
+}
+
+function joinourcommunity(){
+  window.open("https://youtube.com/@ghgltggamer");
+}
+
+function garenaFreeFireD(){
+  window.open("https://www.mediafire.com/file/fuh86q2rtfjp2iy/Garena+Free+Fire+Packing.zip/file");
+}
+function garenaFreeFire(){
+  window.open("GarenaFreeFire.html");
+}
+function ExaGearGold(){
+  window.open("https://www.ghgltggamer.repl.co/exaGearGold.html");
+}
+
+function ExaGearGold_d(){
+  window.open("https://www.mediafire.com/file/0zdoj8kx2dium18/ExaGear_Gold_Packing.zip/file");
+}
